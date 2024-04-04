901e2556100fdfb4f60afef3ddbf405969298030
keshkrish <keshavakrishna2001@gmail.com>
2024-04-04 09:09:00 +0530

    Commenting was done in download.py

--- download.py
+++ download.py
@@ -33,31 +33,31 @@ def select_files(year, num_files):
     for csv in all_csv:
         url=base_url+year+'/'+csv
         df=pd.read_csv(url)
-        if df['MonthlyMeanTemperature'].count()==12: #If we do random sampling, more often 
+        if df['MonthlyMeanTemperature'].count()==12: #If we do random sampling, more often we get files which has null values for all monthly fields. This requirement ensures that we have atleast one monthly field without null values to work with 
             sampled_csv.append(csv)
         if len(sampled_csv)==num_files:
             break
-    if len(sampled_csv)==0:
+    if len(sampled_csv)==0: #if no files satisfy the requirement
         print('No csv files has monthly aggregates!!!')
     else:
-        if len(sampled_csv)<num_files:
+        if len(sampled_csv)<num_files: #if the files satisfying the requirement is less than the required number of files
             print('csv files with monthly aggregates is less than the required number of files. The program will continue to run with the available number of files.')
     
-        with open('selected_random_files','w') as file: #Store the selected random file links in a text file so that the csv files can be fetched using wget
+        with open('selected_random_files','w') as file: #Store the selected file links in a text file so that the csv files can be fetched using wget
             for csv in sampled_csv:
                 file.write(base_url+year+'/'+csv+'\n')
         file.close()
 
-params=read_yaml('params.yaml')
+params=read_yaml('params.yaml') #read parameters
 year,n_locs=str(params['year']),int(params['n_locs'])
 base_url='https://www.ncei.noaa.gov/data/local-climatological-data/access/'
 
-subprocess.run(['wget',base_url+year,'-O',year])
-select_files(year,n_locs)
-if os.path.exists('Data'):
+subprocess.run(['wget',base_url+year,'-O',year]) #fetch the page
+select_files(year,n_locs) #select files
+if os.path.exists('Data'): #delete any previous data if exists
     for file in os.listdir('Data'):
         os.remove(os.path.join('Data',file))
 
-subprocess.run(['wget','--input-file=selected_random_files','-P','Data'])
-os.remove('selected_random_files')
+subprocess.run(['wget','--input-file=selected_random_files','-P','Data']) #fetch the selected data
+os.remove('selected_random_files') #remove unwanted files
 os.remove(year)
